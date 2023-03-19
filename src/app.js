e641f5c85e480bc818b91ba57d5a3ac23ec4fda6
seekopath <>
2023-03-19 16:57:03 +0530

    board with props

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
-export default function Square() {
-    return <button className="square">X</button>;
+function Square({val}) {
+  function onClick(){
+    console.log("clicked button", val)
   }
-  
+
+  return <button className="square" onClick={onClick}>{val}</button>;
+}
+export default function Board(){
+
+  return (
+    <>
+      <div className="row">
+        <Square val="1" />
+        <Square val="2" />
+        <Square val="3" />
+      </div>
+      <div className="row">
+        <Square val="4" />
+        <Square val="5" />
+        <Square val="6" />
+      </div>
+      <div className="row">
+        <Square val="7" />
+        <Square val="8" />
+        <Square val="9" />
+      </div>
+    </>
+  )
+}
