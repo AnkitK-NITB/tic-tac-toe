ea0d58f4d3da6470b42f1749468b83ec0e075a5c
seekopath <>
2023-03-22 17:26:51 +0530

    reset  board and step back

--- src/app.js
+++ src/app.js
@@ -1,47 +1,72 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function Square({value, onClickHandle}) {
-  return <button className="square" onClick={onClickHandle}>{value}</button>
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
-let xturn = true;
-let winner;
-export default function Board(){
-  const [squares,setSquares] = useState(Array(9).fill(null))
-  function handleClick(i){
-    if(winner||squares[i]) return;
-    const newSquares = squares.slice()
-    newSquares[i] = xturn?'X':'O'; 
-    setSquares(newSquares)
-    xturn = 1 - xturn;
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [moves, setMoves] = useState(Array);
+  let xIsNext = (moves.length%2===0);
+  function undo(){
+    if(moves.length===0)return;
+    let cur = moves.pop()
+    const nextSquares = squares.slice();
+    nextSquares[cur] = null;
+    setSquares(nextSquares)
   }
-  winner = calculateWinner(squares)
-  let state;
-  if(winner) state = "Winner: " + winner;
-  else state = (xturn?'X':'O') + "'s turn";
+  function reset(){
+    setSquares(Array(9).fill(null))
+    setMoves([])
+  }
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    moves.push(i)
+    setMoves(moves)
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext?'X':'O';
+    setSquares(nextSquares);
+    // console.log(moves, moves[moves.length-1])
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+  const rowCount = 3, colCount = 3
   return (
     <>
-      <div className="status">{state}</div>
-      <div className="row">
-        <Square value={squares[0]} onClickHandle={()=>handleClick(0)} />
-        <Square value={squares[1]} onClickHandle={()=>handleClick(1)} />
-        <Square value={squares[2]} onClickHandle={()=>handleClick(2)} />
-      </div>
-      <div className="row">
-        <Square value={squares[3]} onClickHandle={()=>handleClick(3)} />
-        <Square value={squares[4]} onClickHandle={()=>handleClick(4)} />
-        <Square value={squares[5]} onClickHandle={()=>handleClick(5)} />
-      </div>
-      <div className="row">
-        <Square value={squares[6]} onClickHandle={()=>handleClick(6)} />
-        <Square value={squares[7]} onClickHandle={()=>handleClick(7)} />
-        <Square value={squares[8]} onClickHandle={()=>handleClick(8)} />
-      </div>
+    <div className="status">{status}</div>
+    <div>
+      {[...new Array(rowCount)].map((x, rowIndex) => {
+        return (
+          <div className="board-row" key={rowIndex}>
+            {[...new Array(colCount)].map((y, colIndex) => <Square key={rowIndex*colCount + colIndex} value={squares[rowIndex*colCount + colIndex]} onSquareClick={() => handleClick(rowIndex*colCount + colIndex)} /> )}
+            
+          </div>
+        )
+      })
+      }
+    </div>
+    <div className='buttons'>
+      <button onClick={()=>undo()}>Step Back</button>
+      <button onClick={()=>reset()}>RESET</button>
+    </div>
     </>
-  )
+  );
 }
 
 function calculateWinner(squares) {
-  const winstates = [
+  const lines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -49,13 +74,11 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  for (let i = 0; i < winstates.length; i++) {
-    const [a, b, c] = winstates[i];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      console.log("winner", squares[a])
-      console.log(a,b,c)
       return squares[a];
     }
   }
